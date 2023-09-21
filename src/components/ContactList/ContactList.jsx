4d7030807c8e44f6c54a69817c0f46a7048c998f
naastichaak <nk01022008@gmail.com>
2023-09-21 21:29:06 +0300

    added animation

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,18 +1,40 @@
 import styles from "./ContactList.module.css";
 
+import { motion, AnimatePresence } from "framer-motion";
 import Button from "../Button/Button";
 
 function ContactList({ contacts, onDelete }) {
   return (
     <ul className={styles.list}>
-      {contacts.map((contact) => (
-        <li className={styles.item} key={contact.id} id={contact.id}>
-          <p className={styles.text}>
-            {contact.name} : {contact.number}
-          </p>
-          <Button onClick={() => onDelete(contact.id)}>Remove</Button>
-        </li>
-      ))}
+      <AnimatePresence>
+        {contacts.map((contact) => (
+          <motion.li
+            initial={{
+              opacity: 0,
+              translateX: "-100%",
+            }}
+            animate={{
+              opacity: 1,
+              translateX: 0,
+            }}
+            exit={{
+              opacity: 0,
+              translateX: "100%",
+            }}
+            transition={{
+              duration: 3,
+            }}
+            className={styles.item}
+            key={contact.id}
+            id={contact.id}
+          >
+            <p className={styles.text}>
+              {contact.name} : {contact.number}
+            </p>
+            <Button onClick={() => onDelete(contact.id)}>Remove</Button>
+          </motion.li>
+        ))}
+      </AnimatePresence>
     </ul>
   );
 }
